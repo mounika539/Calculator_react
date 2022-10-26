9970435473a24b58a47add0dccd55cc60e702b97
Embedded systems <mounikachivakala@gmail.com>
2022-10-26 09:12:46 +0530

    added all

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+//import Calculator from './components/Calculator.js';
+ReactDOM.render(<App/>,document.getElementById('root'));
+
